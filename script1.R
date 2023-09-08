5be086b05ffcbbb1c9fb13a368f9e5a47652b863
mariamreda99 <Mariam.Abdallah@student.uib.no>
2023-09-08 11:42:00 +0200

    New Script

---
+++ script1.R
@@ -0,0 +1,28 @@
+library(tidyverse)
+library(modeldata)
+
+data(ames)
+
+ames %>%
+  group_by(Garage_Type) %>%
+  count(Paved_Drive)
+
+detchd <- ames %>%
+	select(Longitude, Latitude, Sale_Price, Garage_Type, Paved_Drive) %>%
+	filter(Garage_Type == "Detchd")
+
+my_fun <- function(a, b){
+	a_price <- detchd %>%
+		filter(Paved_Drive == a) %>%
+		pull(Sale_Price)
+	
+	b_price <- detchd %>%
+		filter(Paved_Drive == b) %>%
+		pull(Sale_Price)
+	
+	return(
+		t.test(a_price, b_price)
+	)
+}
+
+my_fun("Dirt_Gravel", "Paved")
