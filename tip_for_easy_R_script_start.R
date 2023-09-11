ea941d7bcf165b0af0645c64995be8f1e5742040
BenteSvedSkottvoll <bente.skottvoll@gmail.com>
2023-09-11 11:28:45 +0200

    Draft for easy script starting code ver 1

---
+++ tip_for_easy_R_script_start.R
@@ -0,0 +1,12 @@
+# INFO
+# Purpose: A simple template script to install and load packages with the least code possible 
+# Created: 11.09.2023
+
+# Install package packman for easily and simple code to install and load libraries
+invisible(install.packages(pacman))
+
+# Run pacman to install and load the listed libraries
+pacman::p_load(ggplot2, tidyverse, here)
+
+# View your Rproject home directory
+here()
