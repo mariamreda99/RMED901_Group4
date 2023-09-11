82417e00d3c6270b167212af33bbcdb9c39dd700
BenteSvedSkottvoll <bente.skottvoll@gmail.com>
2023-09-11 11:30:38 +0200

    Added comment in code

--- tip_for_easy_R_script_start.R
+++ tip_for_easy_R_script_start.R
@@ -3,6 +3,7 @@
 # Created: 11.09.2023
 
 # Install package packman for easily and simple code to install and load libraries
+# Invisible inhibits R to write output on console, purpose is to minimize clutter in console
 invisible(install.packages(pacman))
 
 # Run pacman to install and load the listed libraries
